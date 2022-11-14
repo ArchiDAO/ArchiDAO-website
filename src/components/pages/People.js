b08966e2cb1acc54c1de8fae3899229f873d3e93
Barbara Villanova <villanovaarch@gmail.com>
2022-11-14 03:13:35 -0300

    people without connection

--- src/components/pages/People.js
+++ src/components/pages/People.js
@@ -6,13 +6,23 @@ import * as csv from 'csvtojson';
 import React, { useState , useEffect} from 'react';
 import './People.css';
 import Names from '../Names';
+import * as THREE from "three"
+import { Canvas, extend, useFrame, useThree } from "@react-three/fiber"
+import { Physics, useSphere } from "@react-three/cannon"
+import { Sky, Environment, Effects as EffectComposer, useTexture } from "@react-three/drei"
+import { SSAOPass } from "three-stdlib"
 
+extend({ SSAOPass })
 
 csv({
     output: "csv",
   });
   const url =
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vRTSl1iJs_f8SxUon33W-VoCxwnv7vY4VzFwSaCJ7dirEMLA2pvnWmSN4pgylc_S0805Mf9AaTEdJNp/pub?output=csv";
+
+    const rfs = THREE.MathUtils.randFloatSpread
+    const sphereGeometry = new THREE.SphereGeometry(1, 32, 32)
+    const baubleMaterial = new THREE.MeshStandardMaterial({ color: "red", roughness: 0, envMapIntensity: 0.2, emissive: "#370037" })
 
 
 export default function People() {
@@ -79,16 +89,61 @@ export default function People() {
             
             <p className='ppeople' >We are many. These are some.</p>
             <p className='ppeople' >Join us <a href='https://forms.gle/HcWnT64o79awMsfk9' style={{ color: 'chocolate'  }}>here</a> and become a part of the future.</p>
-            
-                <div className="person">
+            </div>
+             <div className="canva-container">
+            <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 20], fov: 35, near: 1, far: 40 }}>
+            <ambientLight intensity={0.25} />
+            <spotLight intensity={1} angle={0.2} penumbra={1} position={[30, 30, 30]} castShadow shadow-mapSize={[512, 512]} />
+            <directionalLight intensity={5} position={[-10, -10, -10]} color="purple" />
+            <Physics gravity={[0, 2, 0]} iterations={10}>
+            <Pointer />
+            <Clump />
+            </Physics>
+            <Environment files="/adamsbridge.hdr" />
+            <Effects />
+            <Sky />
+            </Canvas>
+            </div>
+                {/* <div className="person">
                 {data.map((person) => (
                     <div className='person__data' key={person.nationality}>
                         <h3 >{person.nationality}</h3>
-                    </div>))}
-           </div>
-           </div>
-           <Names />
+                    </div>))} 
+                    </div>*/}
+           
+           
            </>
            
            )
 }
+
+function Clump({ mat = new THREE.Matrix4(), vec = new THREE.Vector3(), ...props }) {
+    const texture = useTexture("/noise.jpg")
+    const [ref, api] = useSphere(() => ({ args: [1], mass: 1, angularDamping: 0.1, linearDamping: 0.65, position: [rfs(20), rfs(20), rfs(20)] }))
+    useFrame((state) => {
+      for (let i = 0; i < 45; i++) {
+        // Get current whereabouts of the instanced sphere
+        ref.current.getMatrixAt(i, mat)
+        // Normalize the position and multiply by a negative force.
+        // This is enough to drive it towards the center-point.
+        api.at(i).applyForce(vec.setFromMatrixPosition(mat).normalize().multiplyScalar(-50).toArray(), [0, 0, 0])
+      }
+    })
+    return <instancedMesh ref={ref} castShadow receiveShadow args={[null, null, 40]} geometry={sphereGeometry} material={baubleMaterial} material-map={texture} />
+  }
+  
+  function Pointer() {
+    const viewport = useThree((state) => state.viewport)
+    const [, api] = useSphere(() => ({ type: "Kinematic", args: [3], position: [0, 0, 0] }))
+    return useFrame((state) => api.position.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 0))
+  }
+  
+  function Effects(props) {
+    const { scene, camera } = useThree()
+    return (
+      <EffectComposer {...props}>
+        <sSAOPass args={[scene, camera, 100, 100]} kernelRadius={1.2} kernelSize={0} />
+      </EffectComposer>
+    )
+  }
+  
