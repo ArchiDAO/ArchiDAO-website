99ea68554f36e23b1c3068c09b6e58e727853010
Barbara Villanova <villanovaarch@gmail.com>
2022-11-14 19:22:13 -0300

    text people

--- src/components/pages/People.js
+++ src/components/pages/People.js
@@ -97,7 +97,7 @@ export default function People() {
         <>
         <div className="people">
             
-            <p className='ppeople' >We are many. These are some.</p>
+            <p className='ppeople' >A 3D tale of who we are.</p>
             <p className='ppeople' >Join us <a href='https://forms.gle/HcWnT64o79awMsfk9' style={{ color: 'chocolate'  }}>here</a> and become a part of the future.</p>
             </div>
              <div className="canva-container">
